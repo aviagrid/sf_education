13c1717f5429fb19e30c6a5957f7902b97fc9415
Ivan Gridchin <avia.grinch@gmail.com>
2021-09-27 23:08:20 +0300

    a random number search algorithm is implemented

--- project_0/game_score.py
+++ project_0/game_score.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numpy import random
 
 
 def random_predict(number:int=1) -> int:
@@ -13,18 +12,31 @@ def random_predict(number:int=1) -> int:
         int: 
             The number of attempts
     """
-    count = 0
+    # Инициализируем границы поиска случайным методом
+    # Так, что бы left_border < number > right_border
+    right_border = 101
+    left_border = 0
+    
+    # Идея алгоритма заключается в постоянном сужении области поиска
+    # предсказываемого числа
+    attempt_cnt = 0
     while True:
-        count += 1  
-        predict_number = np.random.randint(1, 101)
+        attempt_cnt += 1  
+        predict_number = np.random.randint(left_border, right_border+1)
         if number == predict_number:
             break
-        print('...wait...')
-    return (count)
+        elif number > predict_number:
+            if left_border < number and predict_number > left_border:
+                left_border = predict_number
+        elif number < right_border:
+            if right_border > number and predict_number < right_border:
+                right_border = predict_number
+                
+    return (attempt_cnt)
 
 
 def score_game(random_predict) -> int:
-    """Calculating the average number of 1000 guessing attempts by the computer
+    """Calculating the average number of 10000 guessing attempts by the computer
 
     Args:
         random_predict ([function]): 
@@ -36,7 +48,7 @@ def score_game(random_predict) -> int:
     """
     count_ls = []
     np.random.seed(1)
-    random_array = np.random.randint(1, 101, size=(1000))
+    random_array = np.random.randint(1, 101, size=(10000))
     
     for number in random_array:
         count_ls.append(random_predict(number))
